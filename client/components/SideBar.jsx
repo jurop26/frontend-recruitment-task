5e602c8b92a6be88d6bf6b575b0194ba072c2235
Juraj Packa <jurop26@azet.sk>
2026-01-22 14:44:49 +0100

    sideBars buttons added

--- client/components/SideBar.jsx
+++ client/components/SideBar.jsx
@@ -1,3 +1,16 @@
+import { Button } from "./ui/button";
+import { PanelsLeftBottom, Shapes } from "lucide-react";
+
 export default function SideBar() {
-  return <div className="border-r-2 h-screen p-2">SideBar</div>;
+  return (
+    <div className="flex flex-col gap-8 border-r-2 py-8 h-screen ">
+      <Button variant="ghost" directions="vertical">
+        <PanelsLeftBottom /> Projects
+      </Button>
+      <Button variant="ghost" directions="vertical">
+        <Shapes />
+        Notes
+      </Button>
+    </div>
+  );
 }
