d1177c823f024801fc803f2b0a2349eb1ecefb3b
Juraj Packa <jurop26@azet.sk>
2026-01-24 08:57:11 +0100

    dialog, DialogWrapper added

---
+++ client/components/DialogWrapper.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+
+export default function DialogWrapper({
+  trigger,
+  title,
+  description,
+  children,
+}) {
+  const [open, setOpen] = useState(false);
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>{trigger}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        {children}
+      </DialogContent>
+    </Dialog>
+  );
+}
--- client/components/SideBar.jsx
+++ client/components/SideBar.jsx
@@ -1,12 +1,19 @@
-import { Button } from "./ui/button";
 import { PanelsLeftBottom, Shapes } from "lucide-react";
+import { Button } from "./ui/button";
+import DialogWrapper from "./DialogWrapper";
 
 export default function SideBar() {
   return (
     <div className="flex flex-col gap-8 border-r-2 py-8 h-screen ">
-      <Button variant="ghost" directions="vertical">
-        <PanelsLeftBottom /> Projects
-      </Button>
+      <DialogWrapper
+        trigger={
+          <Button variant="ghost" directions="vertical">
+            <PanelsLeftBottom /> Projects
+          </Button>
+        }
+        title="Open / Create new project "
+      ></DialogWrapper>
+
       <Button variant="ghost" directions="vertical">
         <Shapes />
         Notes
